390681965e6357d85c40e7cc74c4d9c4a4d946d9
youfang.xi(席有方) <youfang.xi@transsion.com>
2023-01-17 15:19:38 +0800

    1. 代码整理 2. rom源码下载功能

--- common/src/main/java/me/youfang/common/utils/ProcessExt.kt
+++ common/src/main/java/me/youfang/common/utils/ProcessExt.kt
@@ -1,10 +1,16 @@
 package me.youfang.common.utils
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
 import java.io.File
+import java.util.*
 
 private fun exe(dir: File?, silent: Boolean, vararg commands: String) {
     lds("exe ${commands.joinToString(" ")}", "$dir")
-    val process = ProcessBuilder().command(*commands).redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.INHERIT).directory(dir).start()
+    val fixCommands: List<String> = if (commands.size > 1) commands.toList() else (StringTokenizer(commands[0]) as Enumeration<String>).toList()
+    val process = ProcessBuilder().command(fixCommands).redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.INHERIT).directory(dir).start()
     val shutdownHook = object : Thread() {
         override fun run() {
             ld("process still running and now stopped: ${commands.joinToString(" ")}", "$dir")
@@ -28,6 +34,8 @@ fun exe(dir: File?, vararg commands: String) = exe(dir, false, *commands)
 
 fun exeSilent(dir: File?, vararg commands: String) = exe(dir, true, *commands)
 
+fun exeSilent(vararg commands: String) = exe(null, true, *commands)
+
 fun readCommand(command: String): String {
     val process = Runtime.getRuntime().exec(command)
     val output = process.inputStream.bufferedReader().readText()
@@ -35,28 +43,11 @@ fun readCommand(command: String): String {
     return output
 }
 
-fun exeSimple(command: String, silent: Boolean) {
-    lds("exe", command)
-    val process = Runtime.getRuntime().exec(command)
-    val shutdownHook = object : Thread() {
-        override fun run() {
-            ld("process still running and now stopped", command)
-            process.destroy()
-        }
+private fun BufferedReader.print() {
+    while (true) {
+        val nextChar = read()
+        if (nextChar == -1) break
+        print(nextChar)
     }
-    Runtime.getRuntime().addShutdownHook(shutdownHook)
-    process.inputStream.bufferedReader().lines().forEach {
-        println(it)
-    }
-    process.errorStream.bufferedReader().lines().forEach {
-        println(it)
-    }
-    process.waitFor()
-    Runtime.getRuntime().removeShutdownHook(shutdownHook)
-    lde("exe")
-    if (process.exitValue() != 0) {
-        if (silent) {
-            lw("exe error", command)
-        } else throw IllegalStateException("exe error: $command ")
-    }
+    System.out.flush()
 }
