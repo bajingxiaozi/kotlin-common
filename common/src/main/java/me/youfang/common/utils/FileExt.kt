77a9a873ead60a5f35c249897e6003aaa37d8638
youfang.xi(席有方) <youfang.xi@transsion.com>
2023-04-13 17:12:15 +0800

    1. 更新日志格式

--- common/src/main/java/me/youfang/common/utils/FileExt.kt
+++ common/src/main/java/me/youfang/common/utils/FileExt.kt
@@ -6,3 +6,13 @@ import java.io.File
 fun File.forceMkdir() = apply { FileUtils.forceMkdir(this) }
 
 fun String.toFile() = File(this)
+
+private fun File.nextAvailableDir(): File {
+    if (!exists()) return this
+    var index = 2
+    while (true) {
+        val dir = File(parentFile, "${name}_${index}")
+        if (!dir.exists()) return dir
+        index++
+    }
+}
