24d7721be21fa63346b3775ac029dbd77b2fc35a
youfang.xi(席有方) <youfang.xi@transsion.com>
2023-05-12 15:27:19 +0800

    1. 添加一些常用命令
    Signed-off-by: youfang.xi(席有方) <youfang.xi@transsion.com>

--- common/src/main/java/me/youfang/common/utils/IOExt.kt
+++ common/src/main/java/me/youfang/common/utils/IOExt.kt
@@ -2,6 +2,8 @@ package me.youfang.common.utils
 
 import org.apache.commons.io.FileUtils
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 fun String.writeToFile(file: File) = apply {
     FileUtils.forceMkdirParent(file)
@@ -9,3 +11,7 @@ fun String.writeToFile(file: File) = apply {
 }
 
 fun File.readTextSafe(): String? = kotlin.runCatching { readText() }.getOrNull()
+
+fun File.toOutputStream() = FileOutputStream(this).buffered()
+
+fun File.toInputStream() = FileInputStream(this).buffered()
