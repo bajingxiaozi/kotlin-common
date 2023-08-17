44d4d46134b6a83ef8286bfd364c11b453eb32ef
youfang.xi(席有方) <youfang.xi@transsion.com>
2023-08-17 17:09:32 +0800

    optimize: 整理代码
    Signed-off-by: youfang.xi(席有方) <youfang.xi@transsion.com>

--- common/src/main/java/me/youfang/common/utils/IOExt.kt
+++ common/src/main/java/me/youfang/common/utils/IOExt.kt
@@ -4,14 +4,22 @@ import org.apache.commons.io.FileUtils
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.util.UUID
 
 fun String.writeToFile(file: File) = apply {
     FileUtils.forceMkdirParent(file)
     file.writeText(this)
 }
 
+fun String.writeToFileWithTempFile(file: File) {
+    val tempFile = File(file.parentFile, UUID.randomUUID().toString())
+    writeToFile(tempFile)
+    tempFile.renameTo(file)
+}
+
+
 fun File.readTextSafe(): String? = kotlin.runCatching { readText() }.getOrNull()
 
-fun File.toOutputStream() = FileOutputStream(this).buffered()
+fun File.toOutputStream() = FileOutputStream(this)
 
-fun File.toInputStream() = FileInputStream(this).buffered()
+fun File.toInputStream() = FileInputStream(this)
