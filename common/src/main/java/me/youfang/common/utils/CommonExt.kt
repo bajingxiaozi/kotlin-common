4be430cd850143a4b7c346144c9e88668b5a04d1
youfang.xi(席有方) <youfang.xi@transsion.com>
2023-11-27 18:13:53 +0800

    opt: 1. 整理代码
    Signed-off-by: youfang.xi(席有方) <youfang.xi@transsion.com>

--- common/src/main/java/me/youfang/common/utils/CommonExt.kt
+++ common/src/main/java/me/youfang/common/utils/CommonExt.kt
@@ -6,12 +6,12 @@ fun String.toStringBuilder() = StringBuilder(this)
 
 fun inputWithCheck(hint: String): String {
     print("${hint}->>> ")
-    return readlnOrNull()?.trim()?.takeIf { it.isNotBlank() } ?: throw IllegalArgumentException("should input something")
+    return readlnOrNull()?.trim()?.takeIf { it.isNotBlank() } ?: throw IllegalArgumentException("没有输入任何内容，请确认！！")
 }
 
 fun inputPassword(hint: String): String {
     print("${hint}->>> ")
 //    return System.console().readPassword()
-    return readlnOrNull()?.takeIf { it.isNotBlank() } ?: throw IllegalArgumentException("should input something")
+    return readlnOrNull()?.takeIf { it.isNotBlank() } ?: throw IllegalArgumentException("没有输入任何内容，请确认！！")
 }
 
