edccf149f0ee4efaf9d9f38cbaf00b0247a9bf6b
getting.xi <getting.xi@foxmail.com>
2024-09-01 22:18:57 +0800

    opt: 整理代码，适配compose新版本

--- common/src/main/kotlin/me/youfang/common/ext/SystemExt.kt
+++ common/src/main/kotlin/me/youfang/common/ext/SystemExt.kt
@@ -1,7 +1,11 @@
 package me.youfang.common.ext
 
+import java.io.BufferedReader
 import java.io.IOException
-import java.nio.charset.Charset
+import java.io.InputStreamReader
+
+private val Process.utf8InputReader
+    get() = BufferedReader(InputStreamReader(inputStream, "utf-8"))
 
 fun exe(vararg params: String) {
     println("exe: ${params.joinToString(" ")}")
@@ -14,8 +18,36 @@ fun exe(vararg params: String) {
     Runtime.getRuntime().addShutdownHook(shutdownHook)
     process.waitFor()
     Runtime.getRuntime().removeShutdownHook(shutdownHook)
-    if (process.exitValue() == 0) return
-    throw IOException("exe error ${process.exitValue()}: ${params.joinToString(" ")}")
+    if (process.exitValue() != 0) throw IOException("exe error ${process.exitValue()}: ${params.joinToString(" ")}")
+}
+
+fun exe(vararg params: String, outputCallback: (String) -> Unit) {
+    println("exe: ${params.joinToString(" ")}")
+    val process = ProcessBuilder(*params).redirectErrorStream(true).start()
+    val shutdownHook = object : Thread() {
+        override fun run() {
+            process.destroy()
+        }
+    }
+    Runtime.getRuntime().addShutdownHook(shutdownHook)
+    val reader = process.utf8InputReader
+    val line = StringBuilder()
+    while (true) {
+        val cInt = reader.read()
+        val c = cInt.toChar()
+        if (cInt == -1) break
+        print(c)
+        if (c == '\r' || c == '\n') {
+            outputCallback(line.toString())
+            line.clear()
+        } else {
+            line.append(c)
+        }
+    }
+    println()
+    process.waitFor()
+    Runtime.getRuntime().removeShutdownHook(shutdownHook)
+    if (process.exitValue() != 0) throw IOException("exe error ${process.exitValue()}: ${params.joinToString(" ")}")
 }
 
 fun readCommand(vararg params: String): String {
@@ -27,7 +59,7 @@ fun readCommand(vararg params: String): String {
         }
     }
     Runtime.getRuntime().addShutdownHook(shutdownHook)
-    val consoleOutput = process.inputReader(Charset.forName("utf-8")).readText()
+    val consoleOutput = process.utf8InputReader.readText()
     process.waitFor()
     Runtime.getRuntime().removeShutdownHook(shutdownHook)
     return consoleOutput
